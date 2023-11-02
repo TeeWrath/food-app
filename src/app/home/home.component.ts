c737ab96d3b7cb8ac37a509f76f8c70511fdbfce
TeeWrath <subroto.2003@gmail.com>
2023-11-02 23:13:15 +0530

    add: home component and food service

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FoodService } from '../services/food/food.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  foods !: String[];
+  constructor(private fs : FoodService){}
+  ngOnInit(): void {
+    this.foods = this.fs.getAll();
+  }
+}
