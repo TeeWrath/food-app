cd7801de2c3b58768c28011e3c938e944bc1ba5b
TeeWrath <subroto.2003@gmail.com>
2023-11-03 11:45:08 +0530

    dependency conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { StarRatingComponent } from './components/partials/star-rating/star-rating.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    StarRatingComponent
   ],
   imports: [
     BrowserModule,
