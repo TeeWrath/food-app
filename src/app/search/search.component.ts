0d829018d25a39e4991d0a831bd3da844db580fe
TeeWrath <subroto.2003@gmail.com>
2023-11-03 21:04:47 +0530

    update: added style and router connection to search bar

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -8,7 +9,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class SearchComponent {
   searchTerm : String = "";
-  constructor(private rt : ActivatedRoute){}
+  constructor(private rt : ActivatedRoute, private router : Router){}
 
   ngOnInit(): void {
     this.rt.params.subscribe(params => {
@@ -16,5 +17,11 @@ export class SearchComponent {
         this.searchTerm = params['searchTerm'];
       }
     })
+  }
+
+  search() : void {
+    if (this.searchTerm){
+      this.router.navigateByUrl('/search/' + this.searchTerm)
+    }
   }
 }
