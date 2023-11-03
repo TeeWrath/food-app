6540134a290ed26b737c5f37c7a4bea4c8f5d515
TeeWrath <subroto.2003@gmail.com>
2023-11-03 12:17:37 +0530

    fix: Resolved dependencies and finished food details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { StarRatingComponent } from './components/partials/star-rating/star-rating.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    StarRatingComponent
   ],
   imports: [
     BrowserModule,
